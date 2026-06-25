d377f8591d68885c0ecc3f945b7b149173653e1f
wasd314 <68897049+wasd314@users.noreply.github.com>
2026-06-25 21:29:28 +0900

    ignore

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const path = require("path");
 
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  target: "webworker", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
   mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
